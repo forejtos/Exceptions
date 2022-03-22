10f53fe287fa05027010779e1542762cba278d18
forejt.filip.2018 <forejt.filip.2018@UC313PC07>
2022-03-22 14:09:16 +0100

    Přidejte soubory projektu.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exceptions
+{
+    class Program
+    {
+        public static int ReadInt ()
+        {
+            int ivstup = 0;
+            Console.Write("Zadej první číslo: ");
+            string vstup1 = Console.ReadLine();
+            try
+            {
+                ivstup = int.Parse(vstup1);
+            }
+            catch (FormatException)
+            {
+                if (vstup1 == "exit")
+                    Environment.Exit(0);
+                else
+                    throw new FormatException("Špatná hodnota formátu");
+            }
+
+            return ivstup;
+        }
+        public static int Divide(int num1, int num2)
+        {
+            int k = 0;
+            try { k = num1 / num2; }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Nesmí se dělit nulou");
+            }
+            return k;
+        }
+
+        static void Main(string[] args)
+        {
+            int cislo1;
+            int cislo2;
+
+            do
+            {
+                cislo1 = ReadInt();
+                cislo2 = ReadInt();
+                Console.WriteLine(Divide(cislo1, cislo2);
+            }
+            while (true);
+
+        }
+
+    }
+}
---
+++ Exceptions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Exceptions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exceptions")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("4825dbb6-df73-48d5-8aab-38c77c26cdb5")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
